0527dd611923afd53a79ae3ae10093c64918cd62
sarveshguru <csarvesh288@gmail.com>
2024-02-12 08:58:19 +0530

    screen ratio issue

--- src/App.js
+++ src/App.js
@@ -1,33 +1,33 @@
 import './App.css';
-// import React, { useEffect, useState } from 'react';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+// import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/home/Home';
 import About from './components/about/About';
 import Projects from './components/projects/Projects';
-// import Preloading from './components/preloading/Preloading';
+import Preloading from './components/preloading/Preloading';
 import Contact from './components/contact/Contact';
 import Resume from './components/resume/Resume';
 
 
 function App() {
 
-  // const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(true);
 
-  // useEffect(() => {
-  //   const fetchData = async () => {
-  //     // console.log(loading);
-  //     await new Promise(resolve => setTimeout(resolve, 600000));
+  useEffect(() => {
+    const fetchData = async () => {
+      // console.log(loading);
+      await new Promise(resolve => setTimeout(resolve, 6000));
       
-  //     setLoading(false);
-  //     // console.log(loading);
-  //   };
-  //   fetchData();
-  // }, []);
+      setLoading(false);
+      // console.log(loading);
+    };
+    fetchData();
+  }, []);
 
   return (
     <Router>
-      {/* <Preloading loading={loading} />  */}
+      <Preloading loading={loading} /> 
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="About" element={<About />} />
